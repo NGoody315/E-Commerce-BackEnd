db71b3f095864dde7205726a886b2cf0b87b161a
Nikki G <nikki.goodridge@gmail.com>
2021-03-21 13:38:34 -0400

    fixed some route stuff

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -52,10 +52,8 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-  Category.create({
-    category_name: req.body.category_name
-  })
-  .then(dbCategories => res.json(dbCategories))
+  Category.create(req.body)
+  .then((category) => res.json(category))
   .catch(err => {
     console.log(err);
     res.status(500).json(err)
